2aee3802fa8354fed7efc8132b66a07ebcdeb281
TTaiJin <taeparis980110@gmail.com>
2025-02-18 16:20:51 +0900

    base

---
+++ frontend/next.config.ts
@@ -0,0 +1,36 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "img1.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "img1.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "k.kakaocdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "k.kakaocdn.net",
+      },
+    ],
+    contentSecurityPolicy: "default-src 'self'; img-src 'self' data: https:;",
+  },
+};
+
+export default nextConfig;
