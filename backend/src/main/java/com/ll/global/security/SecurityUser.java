c8e43f54e83b1235ff4ba36c2eeb75590c5dccc3
TTaiJin <taeparis980110@gmail.com>
2025-02-19 17:29:22 +0900

    백, Rq.java 코틀린으로 전환

--- backend/src/main/java/com/ll/global/security/SecurityUser.java
+++ backend/src/main/java/com/ll/global/security/SecurityUser.java
@@ -1,18 +1,19 @@
 package com.ll.global.security;
 
-import lombok.Getter;
+import java.util.Collection;
+import java.util.Map;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
-import java.util.Collection;
-import java.util.Map;
+import lombok.Getter;
 
 public class SecurityUser extends User implements OAuth2User {
     @Getter
-    private long id;
+    public long id;
     @Getter
-    private String nickname;
+    public String nickname;
 
     public SecurityUser(
             long id,
