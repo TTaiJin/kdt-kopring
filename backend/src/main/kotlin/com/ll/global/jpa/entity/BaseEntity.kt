b6f15fdb039428f8270eeac95510bfb152b20b73
TTaiJin <taeparis980110@gmail.com>
2025-02-21 09:24:41 +0900

    백, BaseEntity 끼리 비교할 때 대상이 프록시 객체일 수 있다는 것을 고려

--- backend/src/main/kotlin/com/ll/global/jpa/entity/BaseEntity.kt
+++ backend/src/main/kotlin/com/ll/global/jpa/entity/BaseEntity.kt
@@ -3,11 +3,10 @@ package com.ll.global.jpa.entity
 import com.ll.standard.util.Ut
 import jakarta.persistence.*
 
-
 @MappedSuperclass
 abstract class BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     var _id: Long? = null
 
@@ -15,13 +14,14 @@ abstract class BaseEntity {
         get() = _id ?: 0
 
     val modelName: String
-        get() = Ut.str.lcfirst(this::class.java.simpleName)
+        get() = Ut.str.lcfirst(this::class.simpleName!!)
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other == null || javaClass != other.javaClass) return false
+        if (other == null || other !is BaseEntity) return false
 
-        other as BaseEntity
+        // 아래 비교는 modelName 이 아니라 Hibernate.getClass(other) 를 사용해도 됩니다.
+        if (modelName != other.modelName) return false
 
         return id == other.id
     }
